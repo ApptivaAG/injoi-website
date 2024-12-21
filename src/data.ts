e3f6406161d0fda921b16ab0ac82371d3ceafaef
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2024-12-21 20:02:03 +0100

    Updated team

--- src/data.ts
+++ src/data.ts
@@ -2,53 +2,33 @@ import type { TeamMember } from './types'
 
 export const teamMembers: TeamMember[] = [
   {
-    name: 'Shaban Ademi',
-    image: '/assets/team/shaban-ademi.jpg',
-    role: { de: 'injoi Mitgründer | Marketing & Sales', fr:'CO-Fondateur | Marketing & Ventes', en: 'injoi CO-Founder | Marketing & Sales', it:'' },
-  },
-  {
-    name: 'Hüseyin Ayas',
-    image: '/assets/team/husi-ayas.jpg',
-    role: { de: 'injoi Mitgründer | IT & Software Development', fr:'CO-Fondateur | IT & Software Development', en: 'injoi CO-Founder | IT & Software Development', it:'' },
-  },
-  {
     name: 'Philip Schönholzer',
     image: '/assets/team/philip-schoenholzer-prev.jpg',
-    role: { de: 'Produkt-Entwickler', fr:'Développeur de produit', en: 'Product Developer', it:'' },
+    role: { de: 'Produkt-Entwickler', fr: 'Développeur de produit', en: 'Product Developer', it: '' },
   },
   {
     name: 'Patrik Stutz',
     image: '/assets/team/patrik-stutz-prev.jpg',
-    role: { de: 'Software-Entwickler', fr:'Développeur de logiciels', en: 'Software Developer', it:'' },
+    role: { de: 'Software-Entwickler', fr: 'Développeur de logiciels', en: 'Software Developer', it: '' },
   },
   {
     name: 'Kevin Rickenbach',
     image: '/assets/team/kevin-rickenbach-prev.jpg',
-    role: { de: 'Software-Entwickler', fr:'Développeur de logiciels', en: 'Software Developer', it:'' },
+    role: { de: 'Software-Entwickler', fr: 'Développeur de logiciels', en: 'Software Developer', it: '' },
   },
-  // {
-  //   name: 'Mariana Barbosa',
-  //   image: '/assets/team/mariana-17-small.jpg',
-  //   role: { de: 'Marketing', fr:'Marketing', en: 'Marketing', it:'' },
-  // },
-  // {
-  //   name: 'David Decker',
-  //   image: '/assets/team/david-decker-prev.jpg',
-  //   role: { de: 'Software-Entwickler', en: 'Software Developer' },
-  // },
-  // {
-  //   name: 'Roman Schaller',
-  //   image: '/assets/team/roman-schaller-prev.jpg',
-  //   role: { de: 'Beratung', en: 'Consulting' },
-  // },
-  // {
-  //   name: 'Linus Hüsler',
-  //   image: '/assets/team/linus-huesler-prev.jpg',
-  //   role: { de: 'Beratung', en: 'Consulting' },
-  // },
-  // {
-  //   name: 'Markus Tanner',
-  //   image: '/assets/team/markus-tanner-prev.jpg',
-  //   role: { de: 'Beratung', en: 'Consulting' },
-  // },
+  {
+    name: 'David Decker',
+    image: '/assets/team/david-decker-prev.jpg',
+    role: { de: 'Software-Entwickler', fr: 'Développeur de logiciels', it: '', en: 'Software Developer' },
+  },
+  {
+    name: 'Roman Schaller',
+    image: '/assets/team/roman-schaller-prev.jpg',
+    role: { de: 'Beratung', en: 'Consulting', fr: 'Conseil', it: '' },
+  },
+  {
+    name: 'Linus Hüsler',
+    image: '/assets/team/linus-huesler-prev.jpg',
+    role: { de: 'Beratung', en: 'Consulting', fr: 'Conseil', it: '' },
+  },
 ]
